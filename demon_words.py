c3279163605c7fa3412cc2115e09862d39f4f4e6
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-02-20 16:02:52 -0500

    Renames main.py to mystery_word.py

---
+++ demon_words.py
@@ -0,0 +1,6 @@
+class Bot:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Evil non-sentient opponent. Or is it?"
--- mystery_word.py
+++ mystery_word.py
@@ -3,10 +3,11 @@ import random
 
 class Game:
     def __init__(self):
-        self.start_game()
+        self.word = self.choose_word()
         self.difficulty = 0
         self.player = Player()
-        self.word = self.choose_word()
+        self.playing = True
+        self.start_game()
 
     def __str__(self):
         return "Hangman style game"
@@ -21,29 +22,44 @@ class Game:
         words = self.get_words()
         return random.choice(words)
 
+    def set_difficulty(self):
+        pass
+
     def print_progress(self):
         guesses = self.player.guesses
         hit_list = [char if char in guesses else '_' for char in self.word]
         print(' '.join(hit_list))
 
     def start_game(self):
-        pass
+        self.set_difficulty()
+        print(f"The word contains {len(self.word)} letters.  Good luck chump!")
+        self.rounds()
+
+    def rounds(self):
+        while self.playing:
+            self.player.guess()
+            self.print_progress()
+            guesses = self.player.guesses
+            combined_guesses = ''.join([char for char in self.word if char in guesses])
+            if combined_guesses == self.word:
+                self.player.winner = True
+                self.playing = False
+            elif len(self.player.guesses == 8):
+                self.playing = False
+        self.end_game()
 
     def end_game(self):
-        pass
+        if self.player.winner:
+            print("You win!!!")
+        else:
+            print("You lost in a spectacular fashion.")
 
-
-class Bot:
-    def __init__(self):
-        pass
-
-    def __str__(self):
-        return "Evil non-sentient opponent. Or is it?"
 
 
 class Player:
     def __init__(self):
         self.guesses = []
+        self.winner = False
 
     def __str__(self):
         return "Courageous human word guesser"
@@ -64,5 +80,6 @@ class Player:
         return guess
 
 
+
 if __name__ == "__main__":
-    pass
+    Game()
